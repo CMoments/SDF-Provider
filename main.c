c7f87e335fdaa3275993e0a36f7073116ca13761
CMoments <2319317080@qq.com>
2025-09-25 11:12:54 +0800

    tmp

--- main.c
+++ main.c
@@ -7,7 +7,46 @@
 // #include "swsds.h"
 // Global counters (referenced via extern in testcases.h)
 #include<getopt.h>
-
+int Test_GetDeviceInfo(){
+    int ret = -1;
+    void *hDevice = NULL;
+    void *hSession = NULL;
+    ret = OpenDevice(&hDevice);
+    if (ret != SDR_OK){
+        printf("OpenDevice failed: %s\n", SDF_GetErrorString(ret));
+        goto cleanup;
+    }
+    ret = OpenSession(hDevice, &hSession);
+    if (ret != SDR_OK){
+        printf("OpenSession failed: %s\n", SDF_GetErrorString(ret));
+        goto cleanup;
+    }
+    DEVICEINFO deviceInfo;
+    ret = GetDeviceInfo(hSession, &deviceInfo);
+    if (ret == 0) {
+        printf("\n========== GetDeviceInfo: %s ==========\n", SDF_GetErrorString(ret));
+        printf("IssuerName: %s\n", deviceInfo.IssuerName);
+        printf("SerialNumber: %s\n", deviceInfo.SerialNumber);
+        printf("FirmwareVersion: %s\n", deviceInfo.FirmwareVersion);
+        printf("DeviceVersion: %08x\n", deviceInfo.DeviceVersion);
+        printf("StandardVersion: %d\n", deviceInfo.StandardVersion);
+        printf("AsymAlgAbility: [%08x, %08x]\n", deviceInfo.AsymAlgAbility[0], deviceInfo.AsymAlgAbility[1]);
+        printf("SymAlgAbility: %08x\n", deviceInfo.SymAlgAbility);
+        printf("HashAlgAbility: %08x\n", deviceInfo.HashAlgAbility);
+        printf("BufferSize: %d\n", deviceInfo.BufferSize);
+        printf("===============================================\n");
+    } else {
+        printf("Failed GetDeviceInfo: %s\n", SDF_GetErrorString(ret));
+    }
+cleanup:
+    if(hSession){
+        CloseSession(hSession);
+    }
+    if(hDevice){
+        CloseDevice(hDevice);
+    }
+    return ret;
+}
 int pass = 0;
 int fail = 0;
 int notsupport = 0;
@@ -41,11 +80,7 @@ int main(int argc,char *argv[]){
         }
     }
     if(info_device){
-        DEVICEINFO *device = malloc(sizeof(DEVICEINFO));
-        void *hDevice = NULL;
-        OpenDevice(&hDevice);
-        GetDeviceInfo(hDevice,device);
-        printf("设备厂商: %s\n",device->IssuerName);
+        Test_GetDeviceInfo();
     }
     return 0;
 }
