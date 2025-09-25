a921a5a18527698ee903820dda19e49d4918b323
CMoments <2319317080@qq.com>
2025-09-25 11:08:45 +0800

    temp

--- main.c
+++ main.c
@@ -2,23 +2,50 @@
 #include <dlfcn.h>
 // #include "testcases.h"
 #include "sdf_bind.h"
+#include <stdlib.h>
+#include <string.h>
 // #include "swsds.h"
 // Global counters (referenced via extern in testcases.h)
+#include<getopt.h>
+
 int pass = 0;
 int fail = 0;
 int notsupport = 0;
-int main(){
+int info_device = 0;
+int main(int argc,char *argv[]){
     void *handle = dlopen("./libswsds.so", RTLD_LAZY);
     if (!handle) {
         printf("dlopen error: %s\n", dlerror());
         return 1;
     }
     sdf_bind_init(handle);
-    // Test_all();
-    // printf("\nSummary:\n\033[32m[Pass]=%d\033[0m\n\033[31m[Fail]=%d\033[0m\n\033[33m[NotSupport]=%d\033[0m \n", pass, fail, notsupport);
-    // dlclose(handle);
-    // if(fail==0) {
-    //     printf("All tests passed or not supported.\n");
-    // }
+    int opt;
+    int option_index;
+
+    static struct option long_options[] = {
+        {"device-info", no_argument, NULL, 'i'},
+        // {},
+        // {},
+        // {},
+        {0,0,0,0}
+    };
+
+    while((opt = getopt_long(argc,argv,"i",long_options,&option_index)) != -1){
+        switch (opt)
+        {
+            case 'i':
+                info_device = 1;
+                break;
+            default:
+                exit(1);
+        }
+    }
+    if(info_device){
+        DEVICEINFO *device = malloc(sizeof(DEVICEINFO));
+        void *hDevice = NULL;
+        OpenDevice(&hDevice);
+        GetDeviceInfo(hDevice,device);
+        printf("设备厂商: %s\n",device->IssuerName);
+    }
     return 0;
 }
